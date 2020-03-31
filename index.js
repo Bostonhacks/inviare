96f41b9def0404e55467c4836c24c3b323fa536a
Rudhra Raveendran <rudytheninja@gmail.com>
2020-03-30 21:01:25 -0400

    Added team

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 require('dotenv').config();
+const axios = require('axios');
+const yaml = require('js-yaml');
 const express = require('express');
 const multer  = require('multer')
 const sgMail = require('@sendgrid/mail');
@@ -6,15 +8,23 @@ sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 const app = express();
 const upload = multer();
+const TEAM_EMAILS = [];
+
+axios.get('https://raw.githubusercontent.com/Bostonhacks/squadra/master/team.yml')
+  .then(res => {
+    const doc = yaml.safeLoad(res.data);
+    doc.members.forEach(member => TEAM_EMAILS.push(`${member.name} <${member.mailgun.email}>`));
+    console.log('Team emails set:', TEAM_EMAILS);
+  })
+  .catch(error => console.error(error));
 
 app.get('/', (req, res) => res.send('Parser is running!'));
 
 app.post('/parse', upload.none(), (req, res) => {
-  console.log('Received Email:');
-  console.log(req.body);
+  console.log('Received Email:', req.body);
 
   const msg = {
-    to: 'rooday@bu.edu', // [of bhacks members]
+    to: TEAM_EMAILS,
     from: 'BostonHacks <contact@bostonhacks.io>',
     replyTo: req.body.from,
     subject: req.body.subject,
@@ -23,7 +33,7 @@ app.post('/parse', upload.none(), (req, res) => {
   };
 
   sgMail
-    .send(msg) // sendMultiple()
+    .sendMultiple(msg)
     .then(() => {
       res.send('Email forwarded!');
     })
