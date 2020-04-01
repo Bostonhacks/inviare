509be420232c7a99d9bed276829c7df9c6f009bb
rishabnayak <rishab@bu.edu>
2020-04-01 19:15:09 -0400

    ready v1

--- functions/index.js
+++ functions/index.js
@@ -38,10 +38,7 @@ axios
     })
     .catch(error => console.error(error));
 
-// Easy way to check that this didn't die
-app.get('/', (req, res) => res.send('Parser is running!'));
-
-app.use((req, res, next) => {
+module.exports.parser = functions.https.onRequest((req, res) => {
     const busboy = new Busboy({ headers: req.headers })
     let fileBuffer = new Buffer('')
     req.files = {
@@ -66,91 +63,82 @@ app.use((req, res, next) => {
         })
     })
     busboy.on('finish', () => {
-        next()
+        console.log('Received Email:', req.body);
+
+        // If spam_score is too high, drop it like its porn
+        if (parseFloat(req.body.spam_score) >= parseFloat(process.env.SPAM_THRESHOLD)) {
+            console.log("Received Spam!");
+            res.send("Sorry buddy gonna drop this one");
+        } else {
+            // Grab formatted fromField, fallbock to route
+            const route = JSON.parse(req.body.envelope).to[0];
+            let fromField = route;
+            if (ROUTES.hasOwnProperty(route)) {
+                fromField = ROUTES[route];
+            }
+
+            // Send email only to relevant members
+            const toField = ['Rishab Nayak <rishab@bu.edu>', 'Rudhra Raveendran <rooday@bu.edu>'];//TEAM_EMAILS.filter(member => member.routes.includes(route)).map(member => member.email);
+
+            // Attempt to parse email
+            simpleParser(req.body.email, { skipImageLinks: true })
+                .then(parsed => {
+                    // Parsing success! Use this information to reconstruct email
+                    console.log("Parsed email:", parsed);
+
+                    // Convert parsed attachments to the form SendGrid expects
+                    const attachments = parsed.attachments.map(attachment => {
+                        return {
+                            content: attachment.content.toString('base64'), // MailParser turns content into a buffer, so we need to turn it back to base64
+                            filename: attachment.filename,
+                            type: attachment.contentType,
+                            disposition: attachment.contentDisposition,
+                            contentId: attachment.contentId
+                        }
+                    });
+
+                    return {
+                        from: fromField,
+                        to: toField,
+                        replyTo: parsed.from.text, // Have to access text property as MailParser converts addresses into objects
+                        cc: parsed.cc ? parsed.cc.text : undefined, // These address fields may not exist, so check for that
+                        bcc: parsed.bcc ? parsed.bcc.text : undefined,
+                        subject: parsed.subject,
+                        text: parsed.text,
+                        html: parsed.html ? parsed.html : parsed.textAsHtml, // This also may not exist, so use textAsHtml as fallback
+                        attachments: attachments
+                    };
+                })
+                .catch(error => {
+                    // Parsing failure! Just use the text that SendGrid extracts so we have something
+                    console.error("Failed to parse email:", error);
+                    console.log("Sending email text as fallback");
+
+                    return {
+                        from: fromField,
+                        to: toField,
+                        replyTo: req.body.from,
+                        subject: req.body.subject,
+                        text: req.body.text || '' // Fallback in case the email had no text
+                    };
+                })
+                .then(msg => {
+                    // Regardless of parsing status, we'll have something at this point so send it
+                    sgMail
+                        .sendMultiple(msg)
+                        .then(() => {
+                            console.log('Email forwarded!');
+                            res.send('Email forwarded!');
+                        })
+                        .catch(error => {
+                            console.error('Email forwarding failed:', error); // This is an error within the actual sendgrid-nodejs package
+                            if (error.response) { // This is an error from the SendGrid API
+                                console.error(error.response.body)
+                            }
+                            res.status(500).send('Email forwarding failed!');
+                        });
+                });
+        }
     })
     busboy.end(req.rawBody)
-    req.pipe(busboy)
-})
-
-app.post('/parse', (req, res) => {
-    console.log('Received Email:', req.body);
-
-    // If spam_score is too high, drop it like its porn
-    if (parseFloat(req.body.spam_score) >= parseFloat(process.env.SPAM_THRESHOLD)) {
-        console.log("Received Spam!");
-        res.send("Sorry buddy gonna drop this one");
-    } else {
-        // Grab formatted fromField, fallbock to route
-        const route = JSON.parse(req.body.envelope).to[0];
-        let fromField = route;
-        if (ROUTES.hasOwnProperty(route)) {
-            fromField = ROUTES[route];
-        }
-
-        // Send email only to relevant members
-        const toField = ['Rishab Nayak <rishab@bu.edu>', 'Rudhra Raveendran <rooday@bu.edu>'];//TEAM_EMAILS.filter(member => member.routes.includes(route)).map(member => member.email);
-
-        // Attempt to parse email
-        simpleParser(req.body.email, { skipImageLinks: true })
-            .then(parsed => {
-                // Parsing success! Use this information to reconstruct email
-                console.log("Parsed email:", parsed);
-
-                // Convert parsed attachments to the form SendGrid expects
-                const attachments = parsed.attachments.map(attachment => {
-                    return {
-                        content: attachment.content.toString('base64'), // MailParser turns content into a buffer, so we need to turn it back to base64
-                        filename: attachment.filename,
-                        type: attachment.contentType,
-                        disposition: attachment.contentDisposition,
-                        contentId: attachment.contentId
-                    }
-                });
-
-                return {
-                    from: fromField,
-                    to: toField,
-                    replyTo: parsed.from.text, // Have to access text property as MailParser converts addresses into objects
-                    cc: parsed.cc ? parsed.cc.text : undefined, // These address fields may not exist, so check for that
-                    bcc: parsed.bcc ? parsed.bcc.text : undefined,
-                    subject: parsed.subject,
-                    text: parsed.text,
-                    html: parsed.html ? parsed.html : parsed.textAsHtml, // This also may not exist, so use textAsHtml as fallback
-                    attachments: attachments
-                };
-            })
-            .catch(error => {
-                // Parsing failure! Just use the text that SendGrid extracts so we have something
-                console.error("Failed to parse email:", error);
-                console.log("Sending email text as fallback");
-
-                return {
-                    from: fromField,
-                    to: toField,
-                    replyTo: req.body.from,
-                    subject: req.body.subject,
-                    text: req.body.text || '' // Fallback in case the email had no text
-                };
-            })
-            .then(msg => {
-                // Regardless of parsing status, we'll have something at this point so send it
-                sgMail
-                    .sendMultiple(msg)
-                    .then(() => {
-                        console.log('Email forwarded!');
-                        res.send('Email forwarded!');
-                    })
-                    .catch(error => {
-                        console.error('Email forwarding failed:', error); // This is an error within the actual sendgrid-nodejs package
-                        if (error.response) { // This is an error from the SendGrid API
-                            console.error(error.response.body)
-                        }
-                        res.status(500).send('Email forwarding failed!');
-                    });
-            });
-    }
 });
-
-const parser = functions.https.onRequest(app);
-
-module.exports = { parser }
